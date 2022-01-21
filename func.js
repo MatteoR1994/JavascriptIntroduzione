4737740db03a0f1ca1331c4f205025a3e0ad4f5f
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-01-21 11:36:50 +0100

    fine teoria e inizio esercizi capitolo 3

--- func.js
+++ func.js
@@ -88,3 +88,124 @@ console.log(fibonacci(10));*/
 
 /*********************************************************************************/
 
+function somma(primoNumero, secondoNumero) {
+    return primoNumero + secondoNumero;
+}
+
+let risultato = somma();
+
+console.log(risultato);
+
+/****/
+
+/*function minus(primoNumero, secondoNumero) {
+    if (secondoNumero === undefined) {
+        return -primoNumero;
+    } else {
+        return primoNumero - secondoNumero;
+    }
+}
+
+console.log(minus(20, 5));
+
+console.log(minus(20));*/
+
+/****/
+
+/*function multiplyBy(primoNumero, secondoNumero = 2) {
+    return primoNumero * secondoNumero;
+}*/
+
+//console.log(multiplyBy(6, 3)); // Se metto anche il secondo numero, verrà considerato quello.
+
+//console.log(multiplyBy(6)); // Se non metto il secondo numero, verrà utilizzato quello di default (in questo caso 2).
+
+/****/
+
+/*let variable = -4;
+
+let myFunction;
+
+if (variable >= 0) {
+    let multiplier = 2;
+    myFunction = function (numero) {
+        return numero * multiplier;
+    }
+} else {
+    let multiplier = 3;
+    myFunction = function (numero) {
+        return numero * multiplier;
+    }
+}
+
+let risultato2 = myFunction(4);
+
+console.log(risultato2);*/
+
+// Le funzioni fanno closur = le variabili istanziate in scope non più attivi (dentro un if, per esempio) utilizzate da una funzione vengono mantenute in vita dalla funzione stessa. Vedi sopra.
+
+/*********************************************************************************/
+
+// Le funzioni possono ritornare delle altre funzioni (funzioni di ordine superiore)
+
+/*function multiplyByNumber(moltiplicatore) {
+    return (number) => number * moltiplicatore;
+}
+
+let moltiplicaPer2 = multiplyByNumber(2);
+
+let moltiplicaPer3 = multiplyByNumber(3);
+
+let moltiplicaPer4 = multiplyByNumber(4);
+
+console.log(moltiplicaPer2(5));
+
+console.log(moltiplicaPer3(5));
+
+console.log(moltiplicaPer4(5));*/
+
+/*********************************************************************************/
+
+// Le funzioni possono prendere in pasto altre funzioni
+
+/*function applicaFunzioneSuNumero(mioNumero, funzioneDaApplicare) {
+    return funzioneDaApplicare(mioNumero);
+}
+
+function dividiPer2(numero) {
+    return numero / 2;
+}
+
+function dividiPer3(numero) {
+    return numero / 3;
+}
+
+let res1 = applicaFunzioneSuNumero(8, dividiPer2);
+
+let res2 = applicaFunzioneSuNumero(8, dividiPer3);
+
+console.log(res1);
+
+console.log(res2);*/
+
+/*********************************************************************************/
+
+// ESERCIZI CAPITOLO 3
+
+// 1) Minimum
+
+function minimum(numberOne, numberTwo) {
+    /*if (numberOne < numberTwo) {
+        return numberOne;
+    } else {
+        return numberTwo;
+    }*/
+    return (numberOne < numberTwo) ? numberOne : numberTwo;
+}
+
+console.log(minimum(5, 3));
+
+/****/
+
+// 2) Recursion
+
