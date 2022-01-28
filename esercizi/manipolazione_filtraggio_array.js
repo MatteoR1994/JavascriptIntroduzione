5e0cfbde874bd4a5eaa9afbc64dbe8b653a16230
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-01-28 11:07:23 +0100

    Create manipolazione_filtraggio_array.js

---
+++ esercizi/manipolazione_filtraggio_array.js
@@ -0,0 +1,33 @@
+// ESERCIZIO 1
+
+
+let testArray1 = [12, 57, 1001, -67, -12, 5, 27, 48, -6000];
+
+let testArray2 = ["pippo", "pluto", "paperino", "topolino", "paperone", "paperoga"];
+
+
+/// Teniamo i numeri divisibili per 3;
+let filteredArray1 = 0;  
+
+
+/// Teniamo le stringhe che sono più lunghe di 6 caratteri;
+let filteredArray2 = 0;  
+
+
+/// Tutti i numeri dovranno essere ridotti del 10%
+let mappedArray1
+
+
+/// Tutte le stringhe dovranno essere Maiuscole
+let mappedArray2
+
+
+/// Tutti i numeri andranno trasformati in positivi e andranno eliminati i numeri maggiori di 100
+let modifiedArray1
+
+
+/// Tutte le stringhe dovranno avere la prima lettera maiuscola e andranno eliminate le stringhe che non contengono la lettera 'r'
+let modifiedArray2
+
+
+/*********************************************************************************************************/
