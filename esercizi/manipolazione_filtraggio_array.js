b9fbaf99db4b758a0012f760fcc3f68241c9656a
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-01-28 13:11:01 +0100

    aggiunta reduce con esercizio

--- esercizi/manipolazione_filtraggio_array.js
+++ esercizi/manipolazione_filtraggio_array.js
@@ -6,28 +6,31 @@ let testArray1 = [12, 57, 1001, -67, -12, 5, 27, 48, -6000];
 let testArray2 = ["pippo", "pluto", "paperino", "topolino", "paperone", "paperoga"];
 
 
-/// Teniamo i numeri divisibili per 3;
+/// Teniamo i numeri divisibili per 3; /// FILTER
 let filteredArray1 = 0;  
 
 
-/// Teniamo le stringhe che sono più lunghe di 6 caratteri;
+/// Teniamo le stringhe che sono più lunghe di 6 caratteri; /// FILTER
 let filteredArray2 = 0;  
 
 
-/// Tutti i numeri dovranno essere ridotti del 10%
+/// Tutti i numeri dovranno essere ridotti del 10% /// MAP
 let mappedArray1
 
 
-/// Tutte le stringhe dovranno essere Maiuscole
+/// Tutte le stringhe dovranno essere Maiuscole /// MAP
 let mappedArray2
 
 
-/// Tutti i numeri andranno trasformati in positivi e andranno eliminati i numeri maggiori di 100
+/// Tutti i numeri andranno trasformati in positivi e andranno eliminati i numeri maggiori di 100 /// MAP + FILTER
 let modifiedArray1
 
 
-/// Tutte le stringhe dovranno avere la prima lettera maiuscola e andranno eliminate le stringhe che non contengono la lettera 'r'
+/// Tutte le stringhe dovranno avere la prima lettera maiuscola e andranno eliminate le stringhe che non contengono la lettera 'r' /// MAP + FILTER
 let modifiedArray2
 
+
+/// Quello che viene viene fuori dalla reduce di questo array è la stringa "La casa ha preso fuoco"
+let testArray3 = ["la", "vergogna", "casa", "dannazione", "ha", "pippo", "preso", "secchio", "fuoco"];
 
 /*********************************************************************************************************/
--- manipolazione_array.js
+++ manipolazione_array.js
@@ -1,125 +1,184 @@
-// Primo modo per scrivere una funzione
-function isNegative(element) { // 99.5% programmatori javascript usa questa
-    return element < 0;
-}
+// // Primo modo per scrivere una funzione
+// function isNegative(element) { // 99.5% programmatori javascript usa questa
+//     return element < 0;
+// }
 
 
-// Secondo modo per scrivere una funzione
-let isNegative2 = function(element) {
-    return element < 0;
-}
+// // Secondo modo per scrivere una funzione
+// let isNegative2 = function(element) {
+//     return element < 0;
+// }
 
 
-// Terzo modo per scrivere una funzione
-let isNegative3 = (element) => element < 0; // elemento in input => ciò che sto ritornando
+// // Terzo modo per scrivere una funzione
+// let isNegative3 = (element) => element < 0; // elemento in input => ciò che sto ritornando
 
 
-/** Tutti e tre sono la stessa identica cosa **/
+// /** Tutti e tre sono la stessa identica cosa **/
 
-/** Le ultime due (senza nome di variabile) si scrivono in tempo reale **/
+// /** Le ultime due (senza nome di variabile) si scrivono in tempo reale **/
 
-/** Le funzioni che passeremo al filter avranno non solo l'elemento da controllare ma anche l'indice dell'array **/
-
-
-/*********************************************************************************************************/
+// /** Le funzioni che passeremo al filter avranno non solo l'elemento da controllare ma anche l'indice dell'array **/
 
 
-// FILTER
-let myArray = [100, 101, 9, 1000, 12, -3];
-
-// 1° modo
-function hasEvenPosition(element, index) {
-    if (index % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-console.log(myArray.filter(hasEvenPosition));
-
-/****/
-
-// 2° modo
-console.log(myArray.filter(function(element, index) {
-    return index % 2 === 0;
-}));
-
-/****/
-
-// 3° modo
-console.log(myArray.filter((e, i) => i % 2 === 0));
+// /*********************************************************************************************************/
 
 
-/*****************************************************************************/
+// // FILTER
+// let myArray = [100, 101, 9, 1000, 12, -3];
 
-let myArray2 = [100, 101, 9, 1000, 12, -3];
+// // 1° modo
+// function hasEvenPosition(element, index) {
+//     if (index % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-// MAP
+// console.log(myArray.filter(hasEvenPosition));
 
-// Semplice
-function map(arrayToModify, alterationFunction) {
-    let tempArray = [];
-    for (const element of arrayToModify) {
-        let modifiedElement = alterationFunction(element);
-        tempArray.push(modifiedElement);
-    }
-    return tempArray;
-}
+// /****/
 
-function addOne(element) {
-    return element + 1;
-}
+// // 2° modo
+// console.log(myArray.filter(function(element, index) {
+//     return index % 2 === 0;
+// }));
 
-// function addOneToArray(arrayToModify) {
+// /****/
+
+// // 3° modo
+// console.log(myArray.filter((e, i) => i % 2 === 0));
+
+
+// /*****************************************************************************/
+
+// let myArray2 = [100, 101, 9, 1000, 12, -3];
+
+// // MAP
+
+// // Semplice
+// function map(arrayToModify, alterationFunction) {
 //     let tempArray = [];
 //     for (const element of arrayToModify) {
-//         let modifiedElement = element + 1;
+//         let modifiedElement = alterationFunction(element);
 //         tempArray.push(modifiedElement);
 //     }
 //     return tempArray;
 // }
 
-// console.log(addOneToArray(myArray2));
+// function addOne(element) {
+//     return element + 1;
+// }
 
-console.log("Mia map", map(myArray2, addOne));
+// // function addOneToArray(arrayToModify) {
+// //     let tempArray = [];
+// //     for (const element of arrayToModify) {
+// //         let modifiedElement = element + 1;
+// //         tempArray.push(modifiedElement);
+// //     }
+// //     return tempArray;
+// // }
+
+// // console.log(addOneToArray(myArray2));
+
+// console.log("Mia map", map(myArray2, addOne));
 
 
-console.log("JS map 1", myArray2.map(addOne));
-// =
-console.log("JS map 2", myArray2.map(function(element) {
-    return element + 1;
-}));
-// =
-console.log("JS map 3", myArray.map((element) => element + 1));
+// console.log("JS map 1", myArray2.map(addOne));
+// // =
+// console.log("JS map 2", myArray2.map(function(element) {
+//     return element + 1;
+// }));
+// // =
+// console.log("JS map 3", myArray.map((element) => element + 1));
 
-/****/
+// /****/
 
-// Con index
+// // Con index
 
-let array3 = [3, 2, 5, -4, 1];
+// let array3 = [3, 2, 5, -4, 1];
 
-function elementIndexProduct(element, index) {
-    return element * index;
-}
+// function elementIndexProduct(element, index) {
+//     return element * index;
+// }
 
-console.log(array3.map(elementIndexProduct));
-// =
-console.log(array3.map(function(element, index) {
-    return element * index;
-}));
-// =
-console.log(array3.map((e, i) => e * i));
+// console.log(array3.map(elementIndexProduct));
+// // =
+// console.log(array3.map(function(element, index) {
+//     return element * index;
+// }));
+// // =
+// console.log(array3.map((e, i) => e * i));
 
-/*****************************************************************************/
+// /*****************************************************************************/
 
-/** FILTER e MAP sono concatenabili **/
+// /** FILTER e MAP sono concatenabili **/
 
-console.log(array3.map(elementIndexProduct)
-                  .filter(isNegative));
+// console.log(array3.map(elementIndexProduct)
+//                   .filter(isNegative));
 
 
 /*****************************************************************************/
 
 // REDUCE
 
+let testArray1 = [12, 57, 1001, -67, -12, 5, 27, 48, -6000];
+
+let testArray2 = ["pippo", "pluto", "paperino", "topolino", "paperone", "paperoga"];
+
+
+function sumAllArray(arrayToSum) {
+    let result = 0;
+    for (const element of arrayToSum) {
+        result = result + element;
+    }
+    return result;
+}
+
+console.log(sumAllArray(testArray1));
+
+
+function sum(first, second) {
+    return first + second;
+}
+
+function product(previus, current, index) {
+    return previus * current;
+}
+
+function sumEvenIndex(previous, current, index) {
+    if (index %2 === 0) {
+        return previous + current;
+    } else {
+        return previous;
+    }
+}
+
+
+function reduce(arrayToAggregate, aggregationFunction, startingElement) {
+    let result;
+    let startingIndex;
+    if (startingElement) {
+        result = startingElement;
+        startingIndex = 0;
+    } else {
+        result = arrayToAggregate[0];
+        startingIndex = 1;
+    }
+
+    for (let i = startingIndex; i < arrayToAggregate.length; i++) {
+        const element = arrayToAggregate[i];
+        result = aggregationFunction(result, element, i);        
+    }
+    return result;
+}
+
+console.log(reduce(testArray1, sum));
+console.log(reduce(testArray1, product));
+console.log(reduce(testArray1, sum));
+console.log(reduce(testArray1, sumEvenIndex));
+
+
+
+console.log(testArray1.reduce((previous, current, index, array) => previous + current));
