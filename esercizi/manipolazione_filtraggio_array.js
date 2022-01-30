ec62ad5601e56e775cc6b284b228958a1091c92e
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-01-30 16:31:15 +0100

    fatto esercizio reduce

--- esercizi/manipolazione_filtraggio_array.js
+++ esercizi/manipolazione_filtraggio_array.js
@@ -35,11 +35,67 @@ let testArray3 = ["la", "vergogna", "casa", "dannazione", "ha", "pippo", "preso"
 
 
 function phrase(previous, current, index) {
-    if (index %2 === 0) {
-        
+    if (index % 2 === 0) {
+        if (previous !== undefined) {
+            previous = previous + " " + current;
+        } else {
+            previous = "";
+            previous = previous + " " + current;
+        }
     }
+    return previous
 }
 
-console.log();
+/*
+
+GIRO 1:
+    - previous = undefined // Perché non c'è l'elemento iniziale
+    - current = testArray3[0]
+    - index = 0
+    -- index % 2 === 0 ? SI
+        -- previous != undefined ?
+            - NO: previous = ""; poi diventa stringa vuota + " " + current;
+
+GIRO 2:
+    - previous = "" + testArray3[0] + " "
+    - current = testArray3[1]
+    - index = 1
+    -- index % 2 === 0 ? NO - vado avanti nel ciclo
+
+GIRO 3:
+    - previous = "" + testArray3[0] + " "
+    - current = testArray3[2]
+    - index = 2
+    -- index % 2 === 0 ? SI
+        -- previous != undefined ?
+            - SI: previous = previous + " " + current;
+
+GIRO 4:
+    - previous = "" + testArray3[0] + " " + testArray3[2] + " "
+    - current = testArray[3]
+    - index = 3
+    -- index % 2 === 0 ? NO - vado avanti nel ciclo
+
+e vado avanti così fino alla fine dell'array.
+
+*/
+
+
+//console.log(testArray3.reduce(phrase, ""));
+
+console.log(testArray3.reduce(phrase));
+
+//console.log(testArray3.reduce((p, c, i) => i % 2 !== 0 ? p : p + " " + c));
+
+//console.log(testArray3.reduce((p,c,i) => i%2===0 ? p + " " + c : p));
 
 /*********************************************************************************************************/
+
+let numeri = [1, 2, 3, 4];
+
+function sumArray(previous, current) {
+    previous = previous + current;
+    return previous;
+}
+
+console.log(numeri.reduce(sumArray, 0));
